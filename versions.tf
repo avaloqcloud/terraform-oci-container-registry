8a13d09098775eacda13fb69a392f91efee00f3c
adpa <140618692+u002398@users.noreply.github.com>
2023-10-31 09:19:59 +0100

    Initial commit

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.2.8"
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.18.0"
+    }
+    provider_1 = {
+      source  = "module_1"
+      version = "~> 1.0"
+    }
+    provider_2 = {
+      source  = "module_2"
+      version = "~> 2.5"
+    }
+  }
+}
